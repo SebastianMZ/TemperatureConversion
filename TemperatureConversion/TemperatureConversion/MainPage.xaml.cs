3e450760d335af8c381c85c53c0f061a4222a057
SebastianMZ <sebastian-m.z@hotmail.com>
2017-08-12 04:28:42 -0500

    Version 1 Conversor de temperatura

---
+++ TemperatureConversion/TemperatureConversion/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TemperatureConversion
+{
+    using Xamarin.Forms;
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            ConvertToCsButton.Clicked += ConvertToCsButton_Clicked;
+            ConvertToFhButton.Clicked += ConvertToFhButton_Clicked;
+
+        }
+
+        void ConvertToCsButton_Clicked(object sender, System.EventArgs e)
+        {
+            if (string.IsNullOrEmpty((DegreesEntry.Text)))
+            {
+                DisplayAlert("Error", "You must enter a value in Fahrenheit..", "Accept");
+                return;
+            }
+
+            decimal degrees = 0;
+            if (!decimal.TryParse(DegreesEntry.Text, out degrees))
+            {
+                DisplayAlert("Error", "You must enter a value in degrees..", "Accept");
+                return;
+            }
+            var celsius = (degrees - 32) / 1.8M;
+            CelsiusEntry.Text = string.Format("{0:C2}", celsius);
+
+        }
+
+        void ConvertToFhButton_Clicked(object sender, System.EventArgs e)
+        {
+            if (string.IsNullOrEmpty((DegreesEntry.Text)))
+            {
+                DisplayAlert("Error", "You must enter a value in Celsius", "Accept");
+                return;
+            }
+
+            double degrees = 0;
+            if (!double.TryParse(DegreesEntry.Text, out degrees))
+            {
+                DisplayAlert("Error", "You must enter a value in degrees..", "Accept");
+                return;
+            }
+            double fahrenheit = (degrees * 1.8) + 32D;
+            FahrenheitEntry.Text = string.Format("{0:C2}", fahrenheit);
+        }
+    }
+}
